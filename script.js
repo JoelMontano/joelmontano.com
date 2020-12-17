eefe761305383b57a89614d34cf862019eeac24f
JoelMontano <joel97montano@gmail.com>
2020-12-17 18:05:07 -0500

    did more JS :)

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 document.body.onkeyup = function(event) {
   if (event.keyCode == 68) {
-    // change the class of body to dark mode
     document.body.classList.toggle("dark");
+    var x = document.getElementById("welcometext");
+    if(x.innerHTML === "Press D"){
+      x.innerHTML = "Thanks!";
+    }
+      else{
+        x.innerHTML =  "Press D";
+      }
   }
 }
